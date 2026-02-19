58ff47b216e6040ca77c3b0bfe28db6243337d87
Sara-Fioretti <fiorettisara22@gmail.com>
2026-02-19 16:39:30 +0100

    Creazione card e aggiunta anagrafica

--- script/myscript.js
+++ script/myscript.js
@@ -16,9 +16,11 @@ console.log("Il prezzo del biglietto è "+ prezzo.toFixed(2)+ "€")
 
 let chilometri = document.getElementById ("km")
 let eta = document.getElementById("age")
-let prezzo = document.getElementById("price")
 let bottone = document.getElementById("button")
 let form = document.querySelector ("form")
+let prezzo = document.getElementById("price")
+let anagrafica = document.getElementById ("anagrafica")
+
 
 form.addEventListener ("submit", function(event){
     event.preventDefault();
@@ -37,35 +39,3 @@ form.addEventListener ("submit", function(event){
 
 
 
-/*
-let chilometri = document.getElementById("km")
-let eta = document.getElementById("age")
-let prezzo = document.getElementById("prezzo")
-let bottone = document.getElementById("submit-button")
-let form = document.querySelector("form")
-
-form.addEventListener("submit", function (event) {
-    event.preventDefault();
-}
-  function pricing(eta, prezzo)
-)
-
-function pricing(età, prezzo) {
-    if (eta.value < 18) {
-        prezzo.innerText = ((km * 0.21) - ((km * 0.21) * 0.2))
-    } else if (eta.value > 65) {
-        prezzo.innerText = ((km * 0.21) - (km * 0.21 * 0.4))
-    } else {
-        prezzo.innerText = (km * 0.21)
-    }
-}
-    /*<form action="">
-        <label for="Chilometri">Chilometri da percorrere</label> <br>
-        <input type="text" id="km" placeholder="Inserire i km da percorrere"> <br>
-        <label for="Età">Età del passeggero</label> <br>
-        <input type="text" id="age" placeholder="Inserire l'età del passeggero"><br>
-        <input type="submit" id="submit-button"><br>
-        <label for="price">Prezzo del biglietto</label>
-        <input type="text" id="prezzo">
-    </form>
-*/
